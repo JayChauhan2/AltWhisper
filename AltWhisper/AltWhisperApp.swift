b43a5f8f6285d27827a2d3b6837b7f9890c66f65
JayChauhan2 <jayschauhan3@gmail.com>
2026-04-03 07:22:54 -0400

    Initial Commit

---
+++ AltWhisper/AltWhisperApp.swift
@@ -0,0 +1,17 @@
+//
+//  AltWhisperApp.swift
+//  AltWhisper
+//
+//  Created by Jay Chauhan on 4/3/26.
+//
+
+import SwiftUI
+
+@main
+struct AltWhisperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AltWhisper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AltWhisper
+//
+//  Created by Jay Chauhan on 4/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
